c0ac216ac6d93387dc6401a02c9c96e337868d8f
Vidya <148534500+vidaloi@users.noreply.github.com>
2025-06-09 21:25:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Docker! 🚀"
+    return "Hello Worlds"
 
 @app.route('/about')
 def about():
-    return "This is a simple Flask app running inside a Docker container."
+    return "This is a simple Flask app running inside a Docker container"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
